e5baa6f5e60e2d5f215bf630f7771f455cb75fa7
Ivan Lavriv <Ivan.Lavriv@irco.com>
2017-03-21 18:25:26 +0200

    REmove unused field

--- models/User.go
+++ models/User.go
@@ -6,6 +6,4 @@ type User struct {
 	Password  string `db:"password" json:"password"`
 	FirstName string `db:"first_name" json:"first_name"`
 	LastName  string `db:"last_name" json:"last_name"`
-	// CreatedAt time.Time `db:"created_time" json:"created_at"`
-	// UpdatedAt time.Time `db:"updated_time" json:"updated_at"`
 }
