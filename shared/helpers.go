8fa79075ede3995950bf1159e3f0ab644319d99c
Ivan Lavriv <Ivan.Lavriv@irco.com>
2017-04-04 19:05:38 +0300

    Remove logging from helpers

--- shared/helpers.go
+++ shared/helpers.go
@@ -1,7 +1,6 @@
 package shared
 
 import (
-	"log"
 	"crypto/sha1"
 	"encoding/hex"
 )
@@ -10,6 +9,5 @@ func EncodePassword(password string) string {
 	h := sha1.New()
 	h.Write([]byte(password))
 	encodedPassword := hex.EncodeToString(h.Sum(nil))
-	log.Println(encodedPassword)
 	return encodedPassword
 }
