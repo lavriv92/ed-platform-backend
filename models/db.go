11759e00719a1fc4d56630378a8b3ca68f1b524e
Ivan Lavriv <Ivan.Lavriv@irco.com>
2017-05-03 17:41:25 +0300

    Implement YAML configuration

--- models/db.go
+++ models/db.go
@@ -6,20 +6,22 @@ import (
 	"upper.io/db.v3/lib/sqlbuilder"
 	
 	"upper.io/db.v3/postgresql"
-)
 
-var settings = postgresql.ConnectionURL{
-	Host:     "db:5432",
-	Database: "ed_platform",
-	User:     "ed_platform_user",
-	Password: "ed_platform_password",
-}
+	"app/config"
+)
 
 var session sqlbuilder.Database
 
 func NewSession() (sqlbuilder.Database, error) {
 	if session == nil {
 		var err error
+		cfg := config.NewConfig()
+		settings := postgresql.ConnectionURL{
+			Host:     cfg.Get("database.host"),
+			Database: cfg.Get("database.name"),
+			User:     cfg.Get("database.user"),
+			Password: cfg.Get("database.password"),
+		}
 		session, err = postgresql.Open(settings)
 		if err != nil {
 			log.Printf("Can not connect to database")
